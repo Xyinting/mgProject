11fbdae7989314bac5fa66a6642c99f244274c69
xyt <xilovele@163.com>
2019-10-30 21:32:16 +0800

    正则筛选city

--- crawler/main.go
+++ crawler/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 func main() {
@@ -25,6 +26,23 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%s\n", body)
+	processCity(body)
+}
 
+func processCity(bytes []byte) {
+
+	match := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[a-z0-9]+)"[^>]*>([^<]+)</a>`)
+
+	matches := match.FindAllSubmatch(bytes, -1)
+
+	for _, m := range matches {
+		//for _, submatch := range m {
+		//	fmt.Printf("%s ", submatch)
+		//}
+
+		fmt.Printf("City: %s, URL: %s", m[1], m[2])
+		fmt.Println()
+	}
+
+	fmt.Printf("Matches city number: %d \n ", len(matches))
 }
