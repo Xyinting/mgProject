a4aad99a11b2c78038838471a55c7e672a2d4c22
xyinting <657204162@qq.com>
2019-11-02 10:09:17 +0800

    regex modify

--- regex/regex.go
+++ regex/regex.go
@@ -5,13 +5,18 @@ import (
 	"regexp"
 )
 
-const text = "My email is hh@163.com"
+const text = `My email is hh@163.com
+email is xilovele@qq.com
+email2 is ntoap@nantian.com
+`
 
 func main() {
 
-	compile := regexp.MustCompile(`.+@.+\..+`)
+	compile := regexp.MustCompile(`([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)`)
 
-	match := compile.FindString(text)
-	fmt.Println(match)
-
+	match := compile.FindAllStringSubmatch(text, -1)
+	//fmt.Println(match)
+	for _, m := range match {
+		fmt.Println(m)
+	}
 }
